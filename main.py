76f7dd52c3cc7aabe05f37ee52249265c57f3b8f
Snip <185667147+snipwalter@users.noreply.github.com>
2026-02-03 18:01:41 -0500

    C: sprite, player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
@@ -10,15 +11,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = 1000 / clock.tick(60)
 
 if __name__ == "__main__":
     main()
